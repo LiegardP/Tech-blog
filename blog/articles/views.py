3327f68c1ed7914d75048f742ca9f766fb91c7c9
LiegardP <pierre.liegard@gmail.com>
2019-10-09 22:39:48 +0200

    add articles list and article detail -> urls/views/template

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Article
+from django.http import HttpResponse
 
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('creation_date')
+    return render(request, 'articles/article_list.html', {'articles': articles})
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
